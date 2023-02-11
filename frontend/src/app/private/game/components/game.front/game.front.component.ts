6fcd2e408fce5ab5c2f707773e5229c7164279fd
toto[i] <100564352+demogam1@users.noreply.github.com>
2023-02-11 14:12:31 +0100

    Connected with phaser + almost finished the game logic

--- frontend/src/app/private/game/components/game.front/game.front.component.ts
+++ frontend/src/app/private/game/components/game.front/game.front.component.ts
@@ -1,55 +1,110 @@
 import { Component, OnInit} from '@angular/core';
 import * as Phaser from 'phaser';
 
-class NewScene extends Phaser.Scene {
-  green: any;
-  blue: any;
+class NewScene extends Phaser.Scene 
+{
+  score : any;
+  left_score : number;
+  right_score : number;
+
+  right_pad: any;
+  left_pad: any;
+
+  wall_bottom : any;
+  wall_top : any;
+
+  ball : any;
+  ball_velocity : number;
+
   greenKeys: any;
   blueKeys: any;
 
-  constructor() {
+
+  constructor() 
+  {
     super({ key: 'new' });
+    this.left_score = 0;
+    this.right_score = 0;
+    this.ball_velocity = 0;
+  }
+
+   getRandomInt(min : number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  increase_velocity()
+  {
+    this.ball_velocity + 100;
   }
 
   preload() {
     this.load.setBaseURL('');
-    this.load.image('blueBox', './Green.PNG');
-    this.load.image('greenBox', './Solid_blue.png');
+    this.load.image('right_pad', 'assets/images/pad.png');
+    this.load.image('left_pad', 'assets/images/pad2.png');
+    this.load.image('ball', 'assets/images/ball.png');
   }
 
   create() {
-    this.blue = this.physics.add.image(100, 100, 'blueBox').setCollideWorldBounds(true);
-    this.green = this.physics.add.image(300, 340, 'greenBox').setCollideWorldBounds(true);
+    this.ball_velocity = this.getRandomInt(-200, 200);
+    this.score = this.add.text(850 / 2, 10, this.left_score + ' | ' + this.right_score , { font: '48px Arial'});
 
+    this.wall_bottom = this.add.rectangle(950 / 2, 699, 950, 10 , 0xff0000);
+    this.wall_top = this.add.rectangle(950 / 2, -5, 950, 10 , 0xff0000);
+    
     this.greenKeys = this.input.keyboard.createCursorKeys();
-    this.blueKeys = this.input.keyboard.addKeys('a,s,d,w');
+    this.blueKeys = this.input.keyboard.addKeys('s,w');
+    
+    // Ball config
+    this.ball = this.physics.add.image(950 / 2, 694 / 2, 'ball').setCollideWorldBounds(false);
+    this.ball.setVelocityX(-200, 15);
+    this.ball.scale = 0.03;
+    this.ball.body.setBounce(1,1);
+    //
 
-    this.physics.add.collider(this.green, this.blue, null);
+    // Pads config
+    this.left_pad = this.physics.add.image(30, 350, 'left_pad').setCollideWorldBounds(true);
+    this.right_pad = this.physics.add.image(920, 350, 'right_pad').setCollideWorldBounds(true);
+    this.left_pad.scale = 0.3;
+    this.right_pad.scale = 0.3;
+    this.left_pad.body.pushable = false;
+    this.right_pad.body.pushable = false;
+    //
+    
+    this.physics.add.existing(this.wall_bottom, true); // Ajoute la physique au rectangle cree avec phaser
+    this.physics.add.existing(this.wall_top, true); // Ajoute la physique au rectangle cree avec phaser
+    this.physics.add.existing(this.ball, true);
+    this.physics.add.collider(this.right_pad, this.ball);
+    this.physics.add.collider(this.ball, this.left_pad);
+    this.physics.add.collider(this.ball, this.wall_bottom); // Ajoute la collision entre l'object cree avec phaser et un autre objet
+    this.physics.add.collider(this.ball, this.wall_top);
+    
+    this.input.on('pointermove', function (pointer)
+    {
+      this.left_pad.setVisible(true).setPosition(30, pointer.y);
+    }, this);
+
   }
-
-  override update() {
-    if (this.blueKeys.a.isDown) {
-      this.blue.setVelocityX(-200);
-    } else if (this.blueKeys.d.isDown) {
-      this.blue.setVelocityX(200);
-    } else if (this.blueKeys.w.isDown) {
-      this.blue.setVelocityY(-200);
-    } else if (this.blueKeys.s.isDown) {
-      this.blue.setVelocityY(200);
-    } else if (!this.blue.triggered) {
-      this.blue.setVelocity(0);
+  
+  override update() 
+  {
+    if (this.ball.x > 950)
+    {
+      this.scene.restart();
+      this.left_score++;
     }
-
-    if (this.greenKeys.left.isDown) {
-      this.green.setVelocityX(-200);
-    } else if (this.greenKeys.right.isDown) {
-      this.green.setVelocityX(200);
-    } else if (this.greenKeys.up.isDown) {
-      this.green.setVelocityY(-200);
+    else if (this.ball.x < 0)
+    {
+      this.scene.restart();
+      this.right_score++;
+    }
+    if (this.greenKeys.up.isDown) {
+      this.right_pad.setVelocityY(-200);
     } else if (this.greenKeys.down.isDown) {
-      this.green.setVelocityY(200);
-    } else if (!this.green.triggered) {
-      this.green.setVelocity(0);
+      this.right_pad.setVelocityY(200);
+    } else if (!this.right_pad.triggered) {
+      this.right_pad.setVelocity(0);
     }
   }
 }
@@ -65,14 +120,15 @@ export class GameFrontComponent implements OnInit {
   config: Phaser.Types.Core.GameConfig;
 
   constructor() {
+
     this.config = {
       type: Phaser.AUTO,
-      scene: [ NewScene ],
+      scene: [NewScene],
       scale: {
         mode: Phaser.Scale.FIT,
         parent: 'gameContainer',
-        height: 600,
-        width: 600
+        height: 694,
+        width: 950,
       },
       physics: {
         default: 'arcade',
