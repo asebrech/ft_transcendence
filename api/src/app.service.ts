a58f340d288cf5b63909b7f76d73ed4d34816175
toto[i] <100564352+demogam1@users.noreply.github.com>
2023-02-16 19:04:04 +0100

    implementation du multijoueur en cours , creation de room fini

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,7 +1,11 @@
+import { monitor } from '@colyseus/monitor';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  monitor() {
+    return monitor();
+  }
   getHello(): object {
     return {title: 'Hello There say obiwan!'};
   }
