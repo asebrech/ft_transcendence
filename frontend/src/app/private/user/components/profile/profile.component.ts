908e8b949b9656a6d8e0c82c490a153e9f11864d
Ramzi Fkaïer <ramzifk@hotmail.fr>
2023-02-22 10:08:52 +0100

    profile page html

--- frontend/src/app/private/user/components/profile/profile.component.ts
+++ frontend/src/app/private/user/components/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserI } from 'src/app/model/user.interface';
+import { UserService } from 'src/app/public/services/user-service/user.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +9,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  user : UserI;
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
   }
