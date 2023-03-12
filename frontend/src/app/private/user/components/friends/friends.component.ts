7db5191fb07fe11e74c2be620362721c3d827ba9
Ramzi Fkaïer <ramzifk@hotmail.fr>
2023-03-12 22:39:03 +0100

    ok

--- frontend/src/app/private/user/components/friends/friends.component.ts
+++ frontend/src/app/private/user/components/friends/friends.component.ts
@@ -9,10 +9,30 @@ import { UserService } from 'src/app/public/services/user-service/user.service';
 })
 export class FriendsComponent implements OnInit {
 
+  amis = [
+    {nom: "Alice"},
+    {nom: "Bob"},
+    {nom: "Charlie"},
+    {nom: "David"},
+    {nom: "Eve"}
+  ];
+  suggestions: any[] = [];
+  search : string;
+  displayList: boolean = false;
+
   constructor(private authService: AuthService, private userService: UserService) { }
 
   ngOnInit(): void {
 
   }
 
+  displaySuggests() {
+    const texte = this.search.toLowerCase();
+    this.suggestions = this.amis.filter(ami => ami.nom.toLowerCase().includes(texte));
+  }
+
+  selectionner(ami) {
+    this.search = ami.nom;
+    this.suggestions = [];
+  }
 }
