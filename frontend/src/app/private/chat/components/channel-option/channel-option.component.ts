6deb250c0eca1c1d2161105202c940fb9718d03f
Aloïs Sebrechts <alois@Aloiss-MacBook-Pro.local>
2023-03-14 15:12:19 +0100

    switch branch

--- frontend/src/app/private/chat/components/channel-option/channel-option.component.ts
+++ frontend/src/app/private/chat/components/channel-option/channel-option.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
+import { DashboardService } from '../../services/dashboard-service/dashboard-service';
 
 @Component({
   selector: 'app-channel-option',
   templateUrl: './channel-option.component.html',
   styleUrls: ['./channel-option.component.scss']
 })
-export class ChannelOptionComponent implements OnInit {
+export class ChannelOptionComponent {
 
-  constructor() { }
+	switchMenue: boolean = false;
+	imagePath1 = "../../../../../../assets/images/close.png";
+	imagePath2 = "../../../../../../assets/images/arrow-down-sign-to-navigate.png";
 
-  ngOnInit(): void {
-  }
+	constructor(private elementRef: ElementRef, public dashService: DashboardService) { }
+
+	switch() {
+		this.dashService.channel = !this.dashService.channel;
+		this.hideSwitch();
+	}
+
+	showSwitch() {
+		this.switchMenue = true;
+	}
+
+	hideSwitch() {
+		this.switchMenue = false;
+	}
+
+	@HostListener('document:click', ['$event'])
+	onClick(event: MouseEvent) {
+		if (!this.switchMenue && this.elementRef.nativeElement.querySelector('.arrow').contains(event.target)) {
+			this.showSwitch();
+		}
+		else if (this.switchMenue && !this.elementRef.nativeElement.querySelector('.switch').contains(event.target)) {
+			this.hideSwitch();
+		}
+	}
 
 }
